9fa1a762704e04366408c101c89dbecf75f25352
matiasJoao <joaovictor.a4@gmail.com>
2022-12-06 10:59:16 -0300

    styling

--- components/PrimaryButtons.js
+++ components/PrimaryButtons.js
@@ -1,9 +1,30 @@
-import { View, Text} from 'react-native';
-function PrimaryButton({children}){
-    return(
-        <View>
-           <Text>{children}</Text> 
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+function PrimaryButton({ children }) {
+    return (
+        <View style={styles.buttoncontainer}>
+            <Pressable style={styles.buttoninnerconatainer} android_ripple={{ color: '#00008B' }}>
+                <Text style={styles.textContainer}>{children}</Text>
+            </Pressable>
         </View>
     )
+
 }
 export default PrimaryButton
+
+const styles = StyleSheet.create({
+    buttoncontainer: {
+        borderRadius: 20,
+        margin: 3,
+        overflow: 'hidden',
+    },
+    buttoninnerconatainer: {
+        backgroundColor: '#483D8B',
+        paddingVertical: 4,
+        paddingHorizontal: 7,
+        elevation: 5,
+    },
+    textContainer: {
+        color: '#0000CD',
+        textAlign: 'center'
+    }
+});    
--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -1,12 +1,37 @@
-import { TextInput, View } from 'react-native';
+import { TextInput, View, StyleSheet } from 'react-native';
 import PrimaryButton from '../components/PrimaryButtons';
 function StartScreen (){
     return(
-        <View>
-            <TextInput/>
+        <View style = {styles.inputContainer}>
+            <TextInput style = {styles.numberInput} maxLength={2} keyboardType="number-pad"
+             autoCapitalize='none'
+             autoCorrect={false}/>
             <PrimaryButton>Reset</PrimaryButton>
             <PrimaryButton>Confirm</PrimaryButton>
         </View>
     );
 };
 export default StartScreen;
+
+const styles =  StyleSheet.create({
+    inputContainer:{
+        marginTop: 100,
+        marginHorizontal: 30,
+        borderRadius: 20,
+        padding: 30,
+        backgroundColor: '#191970',
+        elevation: 5,
+    },
+    numberInput: {
+        height: 30,
+        width: 30,
+        fontSize: 20,
+        borderBottomWidth: 1,
+        borderBottomColor: '#4169E1',
+        marginVertical: 6,
+        marginHorizontal: 118,
+        fontWeight: 'bold',
+        color: '#6959CD',
+        textAlign: 'center',
+    }
+});
